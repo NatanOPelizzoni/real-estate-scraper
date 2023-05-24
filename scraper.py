3a85454f842ad6e6340813eefccb34d2020b7ee8
Natan O Pelizzoni <natan2624@gmail.com>
2023-05-24 05:25:16 -0300

    feat: Exporting to csv file, solved some bugs

--- scraper.py
+++ scraper.py
@@ -1,4 +1,5 @@
 import re
+import os
 import math
 import requests
 import pandas as pd
@@ -41,8 +42,9 @@ for group_type in links:
         soup = BeautifulSoup(site.content, 'html.parser')
 
         quantity_text_items = soup.find('h2', {'class': 'titulo-encontrados'}).get_text().strip()
-        index = quantity_text_items.find(' ')
-        quantity_items = quantity_text_items[:index]
+        if(quantity_text_items == 'Nenhum imóvel encontrado'):
+            continue
+        quantity_items = int(quantity_text_items.split(' ')[0])
         last_page = math.ceil(int(quantity_items) / 24)
         
         properties_links = []
@@ -61,17 +63,20 @@ for group_type in links:
             site_property = requests.get(link_property, headers=headers)
             soup_property = BeautifulSoup(site_property.content, 'html.parser')
             
-            div_price = soup_property.find('div', {'class': 'col-price-and-print'})
-            span_price = soup_property.find('span', {'class': 'value-real-number'})
-            if(span_price):
-                property_price = float(
-                    span_price.get_text().strip()
-                    .replace('R$ ', '')
-                    .replace('.', '')
-                    .replace(',', '.')
-                )
-            else:
-                property_price = None
+            div_price = soup_property.find('div', {'class': 'property-pricing'})
+            span_prices = soup_property.findAll('span', {'class': 'value-real-number'})
+            property_price = None
+            if(span_prices):
+                for span_price in span_prices:
+                    if 'value-de' not in span_price['class']:
+                        text = span_price.get_text().strip()
+                        if text.startswith('R$'):
+                            property_price = float(
+                                text.replace('R$', '')
+                                    .replace('.', '')
+                                    .replace(',', '.')
+                            )
+                            break
             
             div_title = soup_property.find('div', {'class': 'property-title'})
             
@@ -80,44 +85,64 @@ for group_type in links:
             
             span_address = div_title.find('span', {'class': 'sp-address-property'}).get_text().strip()
             address_parts = span_address.split(',')
-            local_parts = address_parts[1].split('-')
             
-            city_name = local_parts[1].split('/')[0].strip()
-            abbreviation_uf = local_parts[1].split('/')[1].strip()
-            neighborhood_name = local_parts[0].strip()
-            address = address_parts[0].strip()
-            
+            if len(address_parts) > 2:
+                local_parts = address_parts[2].split('-')
+                address = ','.join([address_parts[0].strip(), address_parts[1].strip().strip()])
+            else:
+                local_parts = address_parts[1].split('-')
+                address = address_parts[0].strip()
+
+            if len(local_parts) > 2:
+                city_name = local_parts[2].split('/')[0].strip()
+                abbreviation_uf = local_parts[2].split('/')[1].strip()
+                neighborhood_name = ' - '.join([local_parts[0].strip(), local_parts[1].strip()])
+            else:
+                city_name = local_parts[1].split('/')[0].strip()
+                abbreviation_uf = local_parts[1].split('/')[1].strip()
+                neighborhood_name = local_parts[0].strip()
+
+            bedrooms_number = None
+            suites_number = None
             string_bedrooms = div_title.find(class_="fa-bed")
             if string_bedrooms:
-                partes_quartos = string_bedrooms.parent.get_text().strip().split('.')
-                bedrooms_number = int(re.findall(r'\d+', partes_quartos[0])[0])
-                if len(partes_quartos[1]) > 0:
-                    suites_number = int(re.findall(r'\d+', partes_quartos[1])[0])
+                bedrooms_parts = string_bedrooms.parent.get_text().strip().split('.')
+                bedrooms_number = int(re.findall(r'\d+', bedrooms_parts[0])[0])
+                if len(bedrooms_parts[1]) > 0:
+                    if 'ou +' not in bedrooms_parts[1]:
+                        suites_number = int(re.findall(r'\d+', bedrooms_parts[1])[0])
 
-            bedrooms_number = bedrooms_number or None
-            suites_number = suites_number or None
-            
             div_characteristics = soup_property.find('div', {'class': 'property-description'})
             
             ul_information = div_characteristics.find('ul', {'class':'listing-features'})
 
+            car_vacancies_number = None
             string_vacancies = ul_information.find('li', {'class':'vagas'})
             if string_vacancies:
                 car_vacancies_number = int(re.findall(r'\d+', string_vacancies.get_text().strip())[0])
-            car_vacancies_number = car_vacancies_number or None
 
+            property_area_size = None
             string_property_area = ul_information.find('li', {'class':'area'})
             if string_property_area:
-                property_area_size = float(
-                    string_property_area.get_text()
-                    .strip()
-                    .split(':')[1]
-                    .replace(' m²', '')
-                    .replace('.', '')
-                    .replace(',', '.')
-                )
-            property_area_size = property_area_size or None
+                text = string_property_area.get_text().strip()
+                if 'Ha' in text:
+                    property_area_size = float(
+                        text
+                        .split(':')[1]
+                        .replace(' Ha', '')
+                        .replace('.', '')
+                        .replace(',', '.')
+                    ) * 10000
+                else:
+                    property_area_size = float(
+                        text
+                        .split(':')[1]
+                        .replace(' m²', '')
+                        .replace('.', '')
+                        .replace(',', '.')
+                    )
 
+            private_property_area = None
             string_private_property_area = ul_information.find('li', {'class':'area-privativa'})
             if string_private_property_area:
                 private_property_area = float(
@@ -128,17 +153,16 @@ for group_type in links:
                     .replace('.', '')
                     .replace(',', '.')
                 )
-            private_property_area = private_property_area or None
 
+            furniture = None
             string_furniture = ul_information.find('li', {'class':'mobilia'})
             if string_furniture:
                 furniture = string_furniture.get_text().split(':')[1].strip()
-            furniture = furniture or None
 
+            floor = None
             string_floor = ul_information.find('li', {'class':'previsao'})
             if string_floor:
                 floor = string_floor.get_text().split(':')[1].strip()
-            floor = floor or None
             
             category_data.append({
                 'real_estate_name':real_estate_name,
@@ -158,3 +182,37 @@ for group_type in links:
             })
 
         data[group_type][category_type] = category_data
+
+df = pd.DataFrame(
+        columns=[
+            'real_estate_name',
+            'group_type',
+            'category_type',
+            'property_price',
+            'property_name',
+            'city_name',
+            'abbreviation_uf',
+            'neighborhood_name',
+            'address',
+            'bedrooms_number',
+            'suites_number',
+            'car_vacancies_number',
+            'property_area_size',
+            'private_property_area',
+            'furniture',
+            'floor'
+        ]
+    )
+
+for group_type in data:
+    for category_type in data[group_type]:
+        category_data = data[group_type][category_type]
+        for item in category_data:
+            df = pd.concat([df, pd.DataFrame(item, index=[0])], ignore_index=True)
+
+output_directory = 'data'
+if not os.path.exists(output_directory):
+    os.makedirs(output_directory)
+
+output_path = os.path.join(output_directory, 'real_estate_data.csv')
+df.to_csv(output_path, encoding='utf-8', sep=';', index=False)
